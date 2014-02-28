b398dfc91120702d67eff9d1efcb333d2fc89062
Matthew McCants <mattmccants@gmail.com>
2014-02-28 16:04:34 -0600

    Recipes and Ingredients are the same thing!

--- recipebook/tests/test_models.py
+++ recipebook/tests/test_models.py
@@ -36,3 +36,18 @@ class RecipeTestCase(TestCase):
 			)
 			self.assertIn(water.ingredient, recipe.ingredients.all())
 			self.assertIn(salt.ingredient, recipe.ingredients.all())
+
+		def test_recipe_as_ingredient(self):
+			pate_fermente = Recipe.objects.create(name='Pate Fermente', method='Mix and rest')
+			french_bread = Recipe.objects.create(name='French Bread', method='Be awesome.')
+			recipe_ct = ContentType.objects.get_for_model(Recipe)
+			ri_pate_fermente = RecipeIngredient.objects.get(
+				content_type__pk=recipe_ct.id,
+				object_id=pate_fermente.id
+			)
+			il_pate_fermente = IngredientLine.objects.create(
+				recipe=french_bread,
+				ingredient=ri_pate_fermente,
+				amount='16 ounces'
+			)
+			self.assertIn(il_pate_fermente.ingredient, french_bread.ingredients.all())
