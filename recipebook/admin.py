6e47bf5fe0f8159e85758f05169323d497248ff8
Matthew McCants <mattmccants@gmail.com>
2014-03-01 21:52:14 -0600

    Recipe Admin Form now filters outs ingredients that are itself.

--- recipebook/admin.py
+++ recipebook/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
 
-from recipebook.models import Ingredient, Recipe, IngredientLine
+from recipebook.models import Ingredient, Recipe, RecipeIngredient, IngredientLine
 
 class IngredientLineInline(admin.TabularInline):
-	model = IngredientLine
+    model = IngredientLine
+    
+    def get_formset(self, request, obj=None, **kwargs):
+        self.parent_obj = obj
+        return super(IngredientLineInline, self).get_formset(request, obj, **kwargs)
+
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        parent_ct = ContentType.objects.get_for_model(self.parent_obj.__class__)
+        recipe_ct = ContentType.objects.get_for_model(Recipe)
+        if parent_ct == recipe_ct:
+            if db_field.name == 'ingredient':
+                kwargs['queryset'] = RecipeIngredient.objects.all().exclude(content_type=recipe_ct, object_id=self.parent_obj.id)
+        return super(IngredientLineInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 class RecipeAdmin(admin.ModelAdmin):
-	inlines = (IngredientLineInline,)
+    inlines = (IngredientLineInline,)
 
 admin.site.register(Ingredient)
 admin.site.register(Recipe, RecipeAdmin)
