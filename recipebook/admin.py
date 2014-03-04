f0daa867fcd0bcadc2d7bf8b7a37e7c21e739dc9
Matthew McCants <mattmccants@gmail.com>
2014-03-04 13:03:37 -0600

    Some PEP8, ignoring E501 for now.

--- recipebook/admin.py
+++ recipebook/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 
-from recipebook.models import Ingredient, Recipe, RecipeIngredient, IngredientLine
+from recipebook.models import (
+    Ingredient, Recipe, RecipeIngredient, IngredientLine
+)
+
 
 class IngredientLineInline(admin.TabularInline):
     model = IngredientLine
-    
+
     def get_formset(self, request, obj=None, **kwargs):
         self.parent_obj = obj
         return super(IngredientLineInline, self).get_formset(request, obj, **kwargs)
-
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         parent_ct = ContentType.objects.get_for_model(self.parent_obj.__class__)
@@ -19,8 +21,10 @@ class IngredientLineInline(admin.TabularInline):
                 kwargs['queryset'] = RecipeIngredient.objects.all().exclude(content_type=recipe_ct, object_id=self.parent_obj.id)
         return super(IngredientLineInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
+
 class RecipeAdmin(admin.ModelAdmin):
     inlines = (IngredientLineInline,)
+
 
 admin.site.register(Ingredient)
 admin.site.register(Recipe, RecipeAdmin)
